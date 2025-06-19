36fb79eedc6f3f4df3d944fcaa68eee8899f8aef
Md. Shahidul Islam <shanto57575@gmail.com>
2025-06-19 13:27:39 +0600

    auth with email verification complete

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,25 +1,35 @@
-import jwt from 'jsonwebtoken';
-import asyncHandler from 'express-async-handler';
-import User from '../models/user.model.js';
+import jwt from "jsonwebtoken";
+import asyncHandler from "express-async-handler";
+import User from "../models/user.model.js";
 
 const protect = asyncHandler(async (req, res, next) => {
   let token;
 
-  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-    try {
-      token = req.headers.authorization.split(' ')[1];
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = await User.findById(decoded.id).select('-password');
-      next();
-    } catch (error) {
-      res.status(401);
-      throw new Error('Not authorized, token failed');
-    }
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  } else if (req.cookies.token) {
+    token = req.cookies.token;
   }
 
   if (!token) {
     res.status(401);
-    throw new Error('Not authorized, no token');
+    throw new Error("Not authorized, no token");
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decoded.id);
+    if (!req.user) {
+      res.status(401);
+      throw new Error("Not authorized, user not found");
+    }
+    next();
+  } catch (error) {
+    res.status(401);
+    throw new Error("Not authorized, token failed");
   }
 });
 
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,17 @@
-import express from 'express';
-import { loginUser, registerUser } from '../controllers/auth.controller.js';
+import express from "express";
+import {
+  registerUser,
+  verifyToken,
+  handleEmailVerification,
+  logout,
+} from "../controllers/auth.controller.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const authRouter = express.Router();
 
-authRouter.post('/register', registerUser);
-authRouter.post('/login', loginUser);
+authRouter.post("/register", registerUser);
+authRouter.post("/verify-token", verifyToken);
+authRouter.post("/verify-email", handleEmailVerification);
+authRouter.post("/logout", protect, logout);
 
 export default authRouter;
