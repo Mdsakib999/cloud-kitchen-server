7ee09ad8b769c2a131aa60a67a64867caa107c67
sadikatanisha <sadikatanisha@gmail.com>
2025-06-24 13:29:35 +0600

    Add: Product and discount models

---
+++ models/product.model.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+const { Schema, model, Types } = mongoose;
+
+const ChoiceSchema = new Schema(
+  {
+    label: { type: String, required: true },
+    price: { type: Number, required: true, min: 0 },
+  },
+  { _id: false }
+);
+
+const OptionGroupSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    type: { type: String, enum: ["single", "multiple"], required: true },
+    required: { type: Boolean, default: false },
+    choices: { type: [ChoiceSchema], required: true },
+  },
+  { _id: false }
+);
+
+const ProductSchema = new Schema(
+  {
+    _id: { type: Number, required: true },
+    title: { type: String, required: true },
+    category: {
+      type: Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    // different discount schema
+    discounts: [
+      {
+        type: Types.ObjectId,
+        ref: "Discount",
+      },
+    ],
+    images: { type: [String], default: [] },
+    sizes: { type: [ChoiceSchema], default: [] },
+    addons: { type: [ChoiceSchema], default: [] },
+    options: { type: [OptionGroupSchema], default: [] },
+    ingredients: { type: [String], default: [] }, // ok to be empty
+    rating: { type: Number, default: 0, min: 0, max: 5 },
+    reviews: { type: Number, default: 0, min: 0 },
+    cookTime: { type: String }, // optional
+    servings: { type: Number, default: 1, min: 1 },
+  },
+  { timestamps: true }
+);
+
+export const Product = model("Product", ProductSchema);
--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -4,8 +4,9 @@ import {
   deleteCategory,
   editCategory,
   getCategories,
-} from "../controllers/admin.controller.js";
+} from "../controllers/category.controller.js";
 import multer from "multer";
+import { createProduct } from "../controllers/product.controller.js";
 const upload = multer();
 
 const adminRouter = express.Router();
@@ -14,5 +15,7 @@ adminRouter.post("/categories", upload.single("image"), addCategory);
 adminRouter.put("/categories/:id", upload.single("image"), editCategory);
 adminRouter.delete("/categories/:id", deleteCategory);
 adminRouter.get("/categories", getCategories);
+// product
+adminRouter.post("/products", upload.array("images"), createProduct);
 
 export default adminRouter;
