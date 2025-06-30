8be91dc47b2d1eaa056a429adc6a922ea4dee47d
Md. Shahidul Islam <shanto57575@gmail.com>
2025-06-30 18:10:01 +0600

    order updating and deleting functionaity added

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -4,17 +4,14 @@ import User from "../models/user.model.js";
 
 const protect = asyncHandler(async (req, res, next) => {
   const authHeader = req.headers.authorization;
-  console.log("authHeader", authHeader);
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.status(401).json({ message: "Not authorized, no token" });
   }
 
   const token = authHeader.split(" ")[1];
-  console.log("token", token);
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = await User.findById(decoded.id);
-    console.log("Inside middleware", decoded, req.user);
 
     if (!req.user) {
       return res
